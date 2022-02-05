53f02bbb87cf4ad0110fec4ff9db442b9fad819f
Cat Book <75529688+CatBook2010@users.noreply.github.com>
2022-02-05 10:58:28 +0000

    made variable youngest and oldest

--- Code.py
+++ Code.py
@@ -1,52 +1,84 @@
+from Person import Person
+
 # We define following class `Person`.
 
 # Do not change this cell but you have to run it to have
 # the Person class in scope.
 
-class Person:
-    """
-    This is a Person class.
-    Each person has a name (str) and a year_of_birth (int).
-    Also, each instance of a Person can compute their own age
-    by calling .compute_age().
-    Printing a person is done calling the __str__ method: we want
-    the name of the person followed by their age in parentheses.
-    """
-    def __init__(self, name, year_of_birth):
-        """
-        Constructor: We have to set the name and year_of_birth.
 
-        name: str
-        year_of_birth: int
-        """
-        self.name = name
-        self.year_of_birth = year_of_birth
+# Use the given Person class to instantantiate a list named persons with the following persons:
 
-    def compute_age(self):
-        """
-        We compute the age of a person by subtracting their
-        year of birth from 2021.
-        """
-        return 2021 - self.year_of_birth
+steve_rogers = Person("Steve Rogers", 1918)
+bucky_barnes = Person("Bucky Barnes", 1917)
+tony_stark = Person("Tony Stark", 1976)
+bruce_banner = Person("Bruce Banner", 1969)
+rhodey_rhodes = Person("Rhodey Rhodes", 1968)
+wanda_maximoff = Person("Wanda Maximoff", 1989)
+vision = Person("Vision", 2015)
+peter_parker = Person("Peter Parker", 2001)
+peter_quill = Person("Peter Quill", 1980)
+monica_rambeau = Person("Monica Rambeau", 1984)
+carol_danvers = Person("Carol Danvers", 1964)
 
-    def __str__(self):
-        """
-        We use a format string to return the Person converted to a string
-        in the following format: "Name (Age)"
-        Example:
-            str(Person("Guido van Rossum", 1956))
-            >>> "Guido van Rossum (65)"
-        """
-        return f"{self.name} ({self.compute_age()})"
+persons = [steve_rogers, bucky_barnes, tony_stark, bruce_banner, rhodey_rhodes, wanda_maximoff, vision, peter_parker, peter_quill, monica_rambeau, carol_danvers]
 
-persons = ['Bucky Barnes, born 1917',
-           'Steve Rogers, born 1918',
-           'Tony Stark, born 1976',
-           'Bruce Banner, born 1969',
-           'Rhodey Rhodes, born 1968',
-           'Wanda Maximoff, born 1989',
-           'Vision, born 2015',
-           'Peter Parker, born 2001',
-           'Peter Quill, born 1980',
-           'Monica Rambeau, born 1984',
-           'Carol Danvers, born 1964']
+
+
+
+# ## Task 1.2: Minimum and Maximum
+# Find the **youngest** and **oldest** person in the given list.
+# Save them in the variables `youngest` and `oldest`. Save the actual instance of the person in the variable and **not** just a string.
+
+age_persons = {}
+
+for person in persons:
+    age_persons[person.compute_age()] = person
+
+youngest = age_persons[min(age_persons.keys())]
+oldest = age_persons[max(age_persons.keys())]
+
+# def get_young_old(persons: list):
+#     youngest = persons[0]
+#     youngest_age = youngest.compute_age()
+#     oldest = persons[0]
+#     oldest_age = oldest.compute_age()
+
+#     for person in persons:
+
+#         if person.compute_age() < youngest_age:
+#             youngest_age = person.compute_age()
+#             youngest = person
+
+#         elif person.compute_age() > oldest_age:
+#             oldest_age = person.compute_age()
+#             oldest = person
+
+#     return youngest, oldest
+
+# youngest, oldest = get_young_old(persons)
+        
+
+## Task 1.3: f-strings
+
+# Use format strings (`f""`) to `print` a table of all `persons` in the following format.
+
+# The result should look like this: 
+
+# ```
+# Name           Year of Birth Age
+# --------------------------------
+# Bucky Barnes            1917 104
+# Steve Rogers            1918 103
+# Tony Stark              1976  45
+# Bruce Banner            1969  52
+# Rhodey Rhodes           1968  53
+# Wanda Maximoff          1989  32
+# Vision                  2015   6
+# Peter Parker            2001  20
+# Peter Quill             1980  41
+# Monica Rambeau          1984  37
+# Carol Danvers           1964  57
+# ```
+
+# (It is enough if it looks roughly the same. As an advanced challege, try to make it look exactly the same: The name must be left-aligned, year of birth and age right-aligned.)
+
